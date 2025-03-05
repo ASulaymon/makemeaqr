5d5bdfe1e5f05b2f273235715de47ab343d4c133
ASulaymon <sulaymonabdusharipov8@gmail.com>
2025-03-05 23:37:47 +0500

    make me a qr

--- js/script.js
+++ js/script.js
@@ -1,59 +1,24 @@
-const text = document.querySelector("input"),
+const url_inp = document.querySelector("input"),
     res = document.querySelector(".result"),
     img = document.querySelector(".QRimg"),
-    buttons = document.querySelectorAll(".btn"),
-    label = document.querySelector(".label"),
-    totext_link = document.querySelector(".totext_link");
+    generate_btn = document.querySelector(".generate"),
+    download_btn = document.querySelector(".download");
 
 setInterval(() => {
-    buttons.forEach((btn) => {
-        if (text.checkValidity() == true) {
-            btn.classList.add("valid");
-        } else {
-            btn.classList.remove("valid");
-        }
-    });
+    if (url_inp.checkValidity() == true) {
+        generate_btn.classList.add("valid");
+    } else {
+        generate_btn.classList.remove("valid");
+    }
 }, 500);
 
-const checkvalid = () => {
-    const input = text.value.trim();
-    let url;
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,7}$/;
-    const domainPattern = /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,7}$/;
-
-    if (emailPattern.test(input)) {
-        return true;
-    }
-
-    if (domainPattern.test(input)) {
-        url = `http://${input}`;
-    } else if (input.startsWith("http://") || input.startsWith("https://")) {
-        url = input;
-    } else {
-        return false;
-    }
-
-    try {
-        new URL(url);
-        return true;
-    } catch (error) {
-        return false;
-    }
-};
-
-let value = text.value.trim();
-
 const generate = () => {
-    if (text.value.length > 0) {
-        if (checkvalid()) {
-            img.src = `https://api.qrserver.com/v1/create-qr-code/?size=1000x1000&data=${text.value}`;
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=${url_inp.value}`;
 
-            res.classList.add("box");
-        } else {
-            alert("Iltimos, URL kiriting");
-        }
-    } else {
-        alert("Iltimos to'ldiring");
+    res.classList.add("box");
+
+    if (img.src.length > 0) {
+        download_btn.classList.add("valid");
     }
 };
 
@@ -68,13 +33,15 @@ const download = () => {
 
             a.href = url;
 
-            a.download = "QR-kod.png";
+            a.download = "QR-kod-makemeaqr.png";
 
             document.body.appendChild(a);
 
             a.click();
 
             document.body.removeChild(a);
+            
+
         })
 
         .catch((error) => console.error("QR kodni yuklashda xato:", error));
